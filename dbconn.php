bda5e1a8702f7e588005d4ce4f3d2f3d3701aa75
Pasan Bopegamage <pasanb2001@gmail.com>
2024-05-06 21:56:08 +0530

    ss

--- dbconn.php
+++ dbconn.php
@@ -1,13 +1,13 @@
 <?php
-// $host='localhost';
-// $user='root';
-// $pass='';
-// $dbname='medtech';
-
 $host = 'localhost';
-$user = 'dreamdes_pasan';
-$pass = 'pasan2020';
-$dbname = 'dreamdes_medtech';
+$user = 'root';
+$pass = '';
+$dbname = 'medtech';
+
+// $host = 'localhost';
+// $user = 'dreamdes_pasan';
+// $pass = 'pasan2020';
+// $dbname = 'dreamdes_medtech';
 
 $conn = mysqli_connect($host, $user, $pass, $dbname);
 if (!$conn) {
